8d0e81e252cf728963523c9689f134acf8b98943
James L <59751464+Jazz1016@users.noreply.github.com>
2022-03-06 21:02:09 -0700

    added components, and some sass

---
+++ town-nav-bar/src/components/App/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../styles/App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <p id="hi-tag">hii</p>
+    </div>
+  );
+};
+
+export default App;
